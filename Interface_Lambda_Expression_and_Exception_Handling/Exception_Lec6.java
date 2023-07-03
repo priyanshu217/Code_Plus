127a776b406de924e0227456f8a22e4b3ee1c58d
Priyanshu <129535434+priyanshu217@users.noreply.github.com>
2023-07-03 08:31:35 +0530

    hello

--- Interface_Lambda_Expression_and_Exception_Handling/Exception_Lec6.java
+++ Interface_Lambda_Expression_and_Exception_Handling/Exception_Lec6.java
@@ -1,34 +1,34 @@
 package Interface_Lambda_Expression_and_Exception_Handling;
-
-import java.lang.reflect.Array;
-
+//
+//import java.lang.reflect.Array;
+//
 public class Exception_Lec6 {
     public static void main(String[] args) {
-        int num1 = 6;
-        int num2 = 2;
-        int num3 = 0;
-        int values = {4,7,2,9};
-        String name = null;
-        try
-        {
-            result = num1 / name.length();
-            System.out.println(values[5]);
-        }
-        catch(ArithmeticException e)
-        {
-            System.out.println("cannot divide by zero" + e);
-        }
-        catch(ArrayIndexOutOfBoundsException e)
-        {
-            System.out.println("stay in your limits");
-        }
-        catch (Exception e)
-        {
-            System.out.println("Something went wrong..");
-        }
-        System.out.println(result);
-        System.out.println("Bye");
-        System.out.println("");
-
+//        int num1 = 6;
+//        int num2 = 2;
+//        int num3 = 0;
+//        int values = {4,7,2,9};
+//        String name = null;
+//        try
+//        {
+//            result = num1 / name.length();
+//            System.out.println(values[5]);
+//        }
+//        catch(ArithmeticException e)
+//        {
+//            System.out.println("cannot divide by zero" + e);
+//        }
+//        catch(ArrayIndexOutOfBoundsException e)
+//        {
+//            System.out.println("stay in your limits");
+//        }
+//        catch (Exception e)
+//        {
+//            System.out.println("Something went wrong..");
+//        }
+//        System.out.println(result);
+//        System.out.println("Bye");
+//        System.out.println("");
+//
     }
 }
