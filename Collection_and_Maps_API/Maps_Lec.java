36c4eddc940bc47f8805d7e0fcd979c54b69a6eb
Priyanshu <129535434+priyanshu217@users.noreply.github.com>
2023-07-11 17:08:00 +0530

    push

--- Collection_and_Maps_API/Maps_Lec.java
+++
@@ -1,7 +0,0 @@
-package Collection_and_Maps_API;
-
-public class Maps_Lec {
-    public static void main(String[] args) {
-        int key
-    }
-}
---
+++ Generics_and_File_Handling/Basics_of_generics.java
@@ -0,0 +1,2 @@
+package Generics_and_File_Handling;public class Basics_of_generics {
+}
