f701987cbdee988a01a7301d94fc3c10d7e8f633
Priyanshu <129535434+priyanshu217@users.noreply.github.com>
2023-07-11 17:53:16 +0530

    main

---
+++ Complete web Development bootcamp/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+9
+	Form.html,a\e\aee612630fec45c5667ec58b71fdbc5ec64f03dc
+>
+Priyanshu.html,f\9\f9d180aa5a412ee1ad867bf47b7b3134db2076dc
--- Generics_and_File_Handling/Basics_of_generics.java
+++ Generics_and_File_Handling/Basics_of_generics.java
@@ -1,2 +1,11 @@
-package Generics_and_File_Handling;public class Basics_of_generics {
+package Generics_and_File_Handling;
+
+public class Basics_of_generics {
+    public static void main(String[] args) {
+        
+
+        
+
+
+    }
 }
