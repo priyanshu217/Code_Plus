3c4ad52ebd5746e9947d827fdfe0a73cc0497765
Priyanshu <129535434+priyanshu217@users.noreply.github.com>
2023-07-13 07:29:42 +0530

    Hello

--- Generics_and_File_Handling/Basics_of_generics.java
+++ Generics_and_File_Handling/Basics_of_generics.java
@@ -1,10 +1,46 @@
 package Generics_and_File_Handling;
+import java.util.*;
 
+ class Gen<T>
+ {
+     T obj;
+     public Gen(T obj)
+     {
+         this.obj=obj;
+     }
+     void disp()
+     {
+         System.out.println("The type of data is :" + obj.getClass().getName());
+     }
+     public T getObj()
+     {
+         return obj;
+     }
+ }
 public class Basics_of_generics {
     public static void main(String[] args) {
-        
+
+        Gen<Integer> g=new Gen<Integer>(10);
+        g.disp();
+        System.out.println(g.getObj());
+
+//        ArrayList<String> list1=new ArrayList<String>();
+//
+//        List<String> list2=new ArrayList<String>();
+//
+//        Collection<Integer> list3=new ArrayList<Integer>();
+//
+//         List<Object> list4=new ArrayList<String>():  ----->  COMPILETIME ERROR
+//
+//        List<Integer> list5=new ArrayList<Integer>();
+//
+//        List<int> list6=new ArrayList<int>();
+
+
+
 
         
+
 
 
     }
