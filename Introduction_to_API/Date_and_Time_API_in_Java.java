829398c9582081c2b6f4955452e7885eefdfb41a
Priyanshu <129535434+priyanshu217@users.noreply.github.com>
2023-07-19 08:12:37 +0530

    com

---
+++ Introduction_to_API/Date_and_Time_API_in_Java.java
@@ -0,0 +1,19 @@
+package Introduction_to_API;
+
+import java.util.Date;
+import java.util.ArrayList;
+
+public class Date_and_Time_API_in_Java {
+    public static void main(String[] args) {
+        java.util.Date dt=new java.util.Date();
+        System.out.println(dt);
+
+        long timeInMs=dt.getTime();
+
+        java.sql.Date dt1=new java.sql.Date();
+        System.out.println(dt1);
+
+
+
+    }
+}
