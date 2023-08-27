0ebe7f565679d702bc2bde849378abca956eda81
Priyanshu <129535434+priyanshu217@users.noreply.github.com>
2023-08-27 21:15:27 +0530

    defd

--- c++/Basics.cpp
+++ c++/Basics.cpp
@@ -3,7 +3,28 @@ using namespace std;
 
 int main () {
 
-    cout << "Mai Pagal huun" <<endl;
-    cout << "Arigatto";
+    // cout << "Mai Pagal huun" <<endl;
+
+    // cout << "Arigatto";
+
+    int a = 123;
+
+    cout<< a << endl;
+
+    char b = 'v';
+    cout<< b << endl;
+
+    bool bl = true;
+    cout<< bl <<endl;
+
+    float f = 1.2;
+    cout<< f << endl;
+
+    double d = 1.235;
+    cout<< d << endl;
+
+    int size = sizeof(a);
+    cout<< "Size of a is :"   << size << endl;
+
 
 }
