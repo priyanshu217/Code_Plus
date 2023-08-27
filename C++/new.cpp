01e8fda137fcd81c0830142082226e8c0a4e2c5e
Priyanshu <129535434+priyanshu217@users.noreply.github.com>
2023-08-27 18:09:39 +0530

    new slash

--- C++/new.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    cout<< "helloPW";
-    return 0;
-}
---
+++ c++/Basics.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    cout << "Mai Pagal huun" <<endl;
+    cout << "Arigatto";
+
+}
