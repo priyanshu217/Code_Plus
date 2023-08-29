fe49ce1e464fbf38c0a54985aecfc5e3f0d0f58b
Priyanshu <129535434+priyanshu217@users.noreply.github.com>
2023-08-29 16:43:03 +0530

    commit

--- c++/Basics.cpp
+++ c++/Basics.cpp
@@ -7,24 +7,44 @@ int main () {
 
     // cout << "Arigatto";
 
-    int a = 123;
+    // int a = 123;
 
-    cout<< a << endl;
+    // cout<< a << endl;
 
-    char b = 'v';
-    cout<< b << endl;
+    // char b = 'v';
+    // cout<< b << endl;
 
-    bool bl = true;
-    cout<< bl <<endl;
+    // bool bl = true;
+    // cout<< bl <<endl;
 
-    float f = 1.2;
-    cout<< f << endl;
+    // float f = 1.2;
+    // cout<< f << endl;
 
-    double d = 1.235;
-    cout<< d << endl;
+    // double d = 1.235;
+    // cout<< d << endl;
 
-    int size = sizeof(a);
-    cout<< "Size of a is :"   << size << endl;
+    // int size = sizeof(a);
+    // cout<< "Size of a is :"   << size << endl;
 
+    // Type Casting
+
+    // int a = 'a';
+    //  cout << a<< endl;
+
+    //  char ch =  99 ;
+    //  cout<< ch << endl;
+
+    //  char ch1 = 123456 ;
+    //  cout<< ch1 << endl;
+
+
+    int a = 5;
+    int b = 6;
+
+    bool first = (a==b);
+    cout<< first << endl;
+     
+    bool second = (a>b);
+    cout<< second << endl;
 
 }
---
+++ c++/If_Else_and_Whileloop.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    //     int a;
+    //     cin>>a;
+
+    //     cout<<" value of n is" << a << endl;
+
+    // int a;
+
+    // if(a>0){
+
+    //     cout<<"A is Positive" << endl;
+
+    // }
+    // else{
+
+    //      cout<<"A is negaive"<< endl;
+    // }
+
+    // int a,b;
+
+    // cout<<"Enter the Value of a " << endl;
+    // cin>>a;
+
+    // cout<<"Enter the Value of b " << endl;
+    // cin>>b;
+
+    // ************** Output Questions *************
+
+    // int a = 9;
+
+    // if (a == 9)
+    // {
+    //     cout << "NINTY";
+    // }
+
+    // if (a > 0)
+    // {
+    //     cout << "POSITIVE";
+    // }
+
+    // else
+    // {
+    //     cout << "NEGATIVE";
+    // }
+
+     int a = 2;
+     int b = a + 1;
+
+     if ((a = 3) == b)
+     {
+         cout << a;
+     }
+
+     else
+     {
+         cout << b;
+     }
+//
+//    int a = 24;
+//    if (a > 20)
+//
+//    {
+//        cout << "Love";
+//    }
+//
+//    else if (a == 24)
+//    {
+//        cout << "Lovely";
+//    }
+//
+//    else
+//    {
+//        cout << "Babbar";
+//    }
+//    cout << a;
+}
